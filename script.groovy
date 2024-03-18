6fc8d27a6b5d8f165b30f882171beb7887558218
Iam-Sai-Phapale <154654109+iam-Sai-Phapale@users.noreply.github.com>
2024-03-19 00:48:14 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -17,18 +17,8 @@ pipeline {
             }
         }
 
-        stage('Test') {
-            steps {
-                sh '/opt/apache-maven-3.6.3/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp-ui  -Dsonar.host.url=http://65.0.168.94:9000 -Dsonar.login=64a4a3e55c70e53044e0f9f42b2739d6d47933dd'
-                echo 'Testing done'
-            }
-        } 
-
-        stage('Deploy') {
-            steps {
-                echo 'Deploy done'
-            }
-        }
-
     }
 }
+
+
+ 
