0e71a11145ecb45933ac9bd2ff7bdd6cc28141a6
Iam-Sai-Phapale <154654109+iam-Sai-Phapale@users.noreply.github.com>
2024-03-19 22:41:24 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -29,7 +29,7 @@ pipeline {
                 script {
                     // Copy built artifacts to EC2 instance
                     sshagent(['ubuntuid']) {
-                            sh 'cat /var/lib/jenkins/workspace/student-app/target/studentapp-2.2-SNAPSHOT.war | ssh ubuntu@ip-172-31-13-217 "cat > /opt/apache-tomcat-9.0.87/webapps/studentapp-2.2-SNAPSHOT.war"'
+                        sh 'rsync -avz -e "ssh -o StrictHostKeyChecking=no" /var/lib/jenkins/workspace/student-app/target/studentapp-2.2-SNAPSHOT.war root@ip-172-31-13-217:/opt/apache-tomcat-9.0.87/webapps/studentapp-2.2-SNAPSHOT.war'
                     }
                 }
             }
