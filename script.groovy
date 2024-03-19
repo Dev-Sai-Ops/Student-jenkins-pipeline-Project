1845892c63198ee719033c443851c86e75924285
Iam-Sai-Phapale <154654109+iam-Sai-Phapale@users.noreply.github.com>
2024-03-19 10:42:49 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -19,7 +19,7 @@ pipeline {
 
         stage('Test') {
             steps {
-                sh '/opt/apache-maven-3.6.3/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp-ui -Dsonar.host.url=http://http://52.66.207.203:9000/ -Dsonar.login=64a4a3e55c70e53044e0f9f42b2739d6d47933dd'
+                sh '/opt/apache-maven-3.6.3/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp-ui -Dsonar.host.url=http://52.66.207.203:9000 -Dsonar.login=75df1c9e34a0449ff40447fe08bd4b8424396605'
                 echo 'Testing done'
             }
         } 
@@ -27,6 +27,7 @@ pipeline {
         stage('Deploy to EC2') {
             steps {
                 script {
+                    // Copy built artifacts to EC2 instance
                     sshagent(['ubuntuid']) {
                         sh 'scp /var/lib/jenkins/workspace/student-app/target/*.war ubuntu@ip-172-31-13-217:/opt/apache-tomcat-9.0.87/webapps'
                     }
@@ -37,3 +38,4 @@ pipeline {
 
     }
 }
+
